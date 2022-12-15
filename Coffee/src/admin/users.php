a037340d9afdadff8c9e1c08429e127af8764fbf
Ulyana <ulyanabelyaeva02@gmail.com>
2022-12-15 18:26:55 +0300

    pr5

--- Coffee/src/admin/users.php
+++ Coffee/src/admin/users.php
@@ -65,7 +65,7 @@ if (!empty($_SESSION['color'])){
     <!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
     <form enctype="multipart/form-data" action="scripts/loadFile.php" method="POST">
         <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
-        <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+        <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
         <!-- Название элемента input определяет имя в массиве $_FILES -->
         Отправить этот файл: <input name="userfile" type="file" />
         <input type="submit" value="Отправить файл" />
@@ -87,7 +87,7 @@ if (!empty($_SESSION['color'])){
     <form action="/logout" method="POST"><button type="submit">Выйти</button></form>
 
     <?php
-        #phpinfo();
+        phpinfo();
     ?>
 </body>
 </html>
